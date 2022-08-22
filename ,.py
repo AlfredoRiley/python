86e9e4067bd486177046bf9f0f347263f6f319d8
AlfredoRiley <111813780+AlfredoRiley@users.noreply.github.com>
2022-08-22 19:57:50 -0300

    aloh

---
+++ ,.py
@@ -0,0 +1,18 @@
+print("ejencicio3 punt final")
+
+print("Numeros de aciertos: ")
+RC=int(input())
+print("Numero de errores: ")
+RI=int(input())
+print("Numero de respuestas en blanco: ")
+RB=int(input())
+
+PCR=RC*3
+PRI=RI*-1
+PRB=RB*0
+
+PF=PCR+PRI+PRB
+
+print("EL puntaje total es: ",PF)
+
+input()
