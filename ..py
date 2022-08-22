9467d53bd1b28e0d18deb14fa49a1f920238cad3
AlfredoRiley <111813780+AlfredoRiley@users.noreply.github.com>
2022-08-22 20:35:23 -0300

    renombre

--- ..py
+++
@@ -1,9 +0,0 @@
-print("Ingresa las notas parciales")
-N1=int(input("Nota 1: "))
-N2=int(input("Nota 2: "))
-N3=int(input("Nota 3: "))
-
-P=(N1+N2+N3)/3
-
-print(P)
-input(P)
---
+++ ejer2.py
@@ -0,0 +1,9 @@
+print("Ingresa las notas parciales")
+N1=int(input("Nota 1: "))
+N2=int(input("Nota 2: "))
+N3=int(input("Nota 3: "))
+
+P=(N1+N2+N3)/3
+
+print(P)
+input(P)
