7eb4dbfd2a75d23390b5dc2c8af1dff5997dbba6
AlfredoRiley <111813780+AlfredoRiley@users.noreply.github.com>
2022-08-22 21:12:49 -0300

    a

--- ejer5.py
+++ ejer5.py
@@ -1,13 +1,13 @@
 import math
 
 print("ingrese GB: ")
-GB=float(print())
+GB = float(input())
 
 MG=GB*1024
-MD=MG/1,44
+MD=MG/1.44
 
 print("SALIDA: ")
 print("---------------------------------")
 print(MD)
 
-print("Numero de discos necesarios ",math.ceil(MD))
+print("Numero de discos necesarios: ", math.ceil(MD))
